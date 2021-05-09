8288bf4fb0d0385ac4b51e48241c4c7f224c1df1
treeplayer14 <tiago_camargo2000@hotmail.com>
2021-05-09 15:29:58 -0300

    instalações e começo feito

---
+++ fetch-git/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	const [name, setName] = useState('');
+	const [userName, setUsername] = useState('');
+	const [followers, setFollowers] = useState('');
+	const [following, setFollowing] = useState('');
+	const [repos, setRepos] = useState('');
+	const [avatar, setAvatar] = useState('');
+	const [userInput, setUserInput] = useState('');
+	const [error, setError] = useState('');
+	
+	return (
+		<div>
+			<h1>hello</h1>
+		</div>
+	);
+	
+}
+
+export default App;
