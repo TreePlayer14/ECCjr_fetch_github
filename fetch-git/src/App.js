9ca0852bee668f1817de3629a44f56e302134fe8
treeplayer14 <tiago_camargo2000@hotmail.com>
2021-05-17 10:33:43 -0300

    codigo terminado mas tem erro

--- fetch-git/src/App.js
+++ fetch-git/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Form, Card, Image, Icon } from 'semantic-ui-react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,11 +11,85 @@ function App() {
 	const [repos, setRepos] = useState('');
 	const [avatar, setAvatar] = useState('');
 	const [userInput, setUserInput] = useState('');
-	const [error, setError] = useState('');
+	const [error, setError] = useState(null);
+	
+	useEffect(() => {
+		fetch('https://api.github.com/users/example')
+		.then(res => res.json())
+		.then(data => {
+			setData(data);
+		});
+	}, []);
+	
+	const setData = ({name, login, followers, following, public_repos, avatar_url}) => {
+		setName(name);
+		setUsername(login);
+		setFollowers(followers);
+		setFollowing(following);
+		setRepos(public_repos);
+		setAvatar(avatar_url);
+	};
+	
+	const handleSearch = e => {
+		setUserInput(e.target.value);
+	};
+	
+	const handleSubmit = () => {
+		fetch('https://api.github.com/users/${userInput}')
+			.then(res => res.json())
+			.then(data => {
+				if(data.message){
+					setError(data.message)
+				}
+				else{
+					setData(data);
+					setError(null);
+				}
+		})
+	};
 	
 	return (
 		<div>
-			<h1>hello</h1>
+			<div className="navbar">Github Search</div>
+			<div className="search">
+				<Form onSubmit={handleSubmit}>
+					<Form.Group>
+						<Form.Input placeholder='Github user' name='github user' onChange={handleSearch}/>
+						<Form.Button content='Search'/>
+					</Form.Group>
+				</Form>
+			</div>
+			{ error ? (<h1>{error}</h1>
+			) : (
+				<div className='card'>
+					<Card>
+						<Image src={avatar} wrapped ui={false} />
+						<Card.Content>
+						  <Card.Header>{name}</Card.Header>
+						  <Card.Header>{userName}</Card.Header>
+						</Card.Content>
+						<Card.Content extra>
+						  <a>
+							<Icon name='user' />
+							{followers} Followers
+						  </a>
+						</Card.Content>
+						<Card.Content extra>
+						  <a>
+							<Icon name='user' />
+							{repos} Repos
+						  </a>
+						</Card.Content>
+						<Card.Content extra>
+						  <a>
+							<Icon name='user' />
+							{following} Following
+						  </a>
+						</Card.Content>
+					</Card>
+				</div>
+			)}
+			
 		</div>
 	);
 	
